380be7ac6324ef250a84d395bf062bfcdaf71c7c
Trincheira - Comunicação Visual <48634870+marvitphy@users.noreply.github.com>
2021-01-19 15:03:32 -0300

    asdf

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const bodyparser = require('body-parser')
+const path = require('path')
+const app = express()
+const mercadopago = require('mercadopago');
+
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
+app.use(express.static('public'))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+// Configura credenciais
+mercadopago.configure({
+    access_token: 'APP_USR-2403643030781235-011820-4bce7bf54617a032c3f98ec07d4c7a3e-412855985'
+
+});
+
+var port = Number(process.env.PORT || 3000);
+
+
+app.get('/', function(req, res) {
+    res.render('Dashboard')
+})
+
+app.get('/sos', function(req, res) {
+    res.render('Sos')
+})
+
+app.get('/pagar', function(req, res) {
+
+    // Cria um objeto de preferência
+    let preference = {
+        items: [{
+            title: 'Meu produto',
+            unit_price: 100,
+            quantity: 1,
+        }]
+    };
+
+    mercadopago.preferences.create(preference)
+        .then(function(response) {
+            // Este valor substituirá a string "<%= global.id %>" no seu HTML
+            global.id = response.body.id;
+        }).catch(function(error) {
+            console.log(error);
+        });
+
+    res.render('Pagar')
+
+})
+
+app.listen(port, function(error) {
+    if (error) throw error
+    console.log("Server created Successfully")
+})
